2abac93aa6477b042f78fe34030cec8e9baff9c1
corlino <corlinoneill@gmail.com>
2025-07-29 10:26:23 -0700

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,10 @@ export default function Head() {
       <title>Colin O'Neill Designs</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/site.webmanifest" />
     </>
   );
 }
