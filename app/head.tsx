8ed41fe2dd48ceb48dbbd24b7298ef1cec3b9534
corlino <corlinoneill@gmail.com>
2025-07-29 09:56:58 -0700

    favicon

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>Colin O'Neill Designs</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
